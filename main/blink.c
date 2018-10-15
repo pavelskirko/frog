971d49041f83b8c06f758fb78d7af29c8ab1ecc8
Pavel <pavel.skirko@gmail.com>
2018-10-15 03:00:08 +0300

    Now logging to the host through JTAG is working fine

--- main/blink.c
+++ main/blink.c
@@ -1,7 +1,6 @@
+
 /* Blink Example
-
    This example code is in the Public Domain (or CC0 licensed, at your option.)
-
    Unless required by applicable law or agreed to in writing, this
    software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
    CONDITIONS OF ANY KIND, either express or implied.
@@ -15,11 +14,13 @@
 #include "esp_log.h"
 #include "esp_wifi.h"
 #include "esp_event_loop.h"
+#include "nvs_flash.h"
 
 /* Can run 'make menuconfig' to choose the GPIO to blink,
    or you can edit the following line and set a number here.
 */
 #define BLINK_GPIO 27
+#define MAX_APs 20
 
 
 static const char* TAG = "Blink";
@@ -46,9 +47,8 @@ void blink_task(void *pvParameter)
 
     	gpio_set_level(BLINK_GPIO, 0);
 //        esp_log_set_vprintf(esp_apptrace_vprintf);
-        ESP_LOGI(TAG, "12345");
-        esp_log_set_vprintf(esp_apptrace_vprintf);
-        esp_apptrace_flush(ESP_APPTRACE_DEST_TRAX, 100000);
+//        esp_apptrace_flush(ESP_APPTRACE_DEST_TRAX, 100000);
+//        ESP_LOGI(TAG, "hello world!");
         vTaskDelay(100 / portTICK_PERIOD_MS);
         /* Blink on (output high) */
         gpio_set_level(BLINK_GPIO, 1);
@@ -56,25 +56,50 @@ void blink_task(void *pvParameter)
     }
 }
 
-void app_main()
+void print_to_host(char *msg, ...)
 {
-//	tcpip_adapter_init();
-//	esp_event_loop_init(event_handler, NULL);
-//	wifi_init_config_t wifi_config = WIFI_INIT_CONFIG_DEFAULT();
-//	esp_wifi_init(&wifi_config);
-//	esp_wifi_set_mode(WIFI_MODE_STA);
-//	esp_wifi_start();
-//	wifi_scan_config_t scan_config = {
-//		.ssid = 0,
-//		.bssid = 0,
-//		.channel = 0,
-//	        .show_hidden = true
-//	};
-//	esp_wifi_scan_start(&scan_config, true);
-//	uint16_t ap_num = 10;
-//	wifi_ap_record_t ap_records[10];
-//	esp_wifi_scan_get_ap_records(&ap_num, ap_records);
+	va_list args;
+	va_start(args, msg);
+	esp_apptrace_vprintf(msg, args);
+	va_end(args);
+	esp_apptrace_flush(ESP_APPTRACE_DEST_TRAX, 100000);
+}
 
-	esp_apptrace_init();
+static char* getAuthModeName(wifi_auth_mode_t auth_mode) {
+
+	char *names[] = {"OPEN", "WEP", "WPA PSK", "WPA2 PSK", "WPA WPA2 PSK", "MAX"};
+	return names[auth_mode];
+}
+
+void app_main()
+
+{
+	esp_log_set_vprintf(esp_apptrace_vprintf);
+	ESP_ERROR_CHECK(nvs_flash_init());
+	tcpip_adapter_init();
+	ESP_ERROR_CHECK ( esp_event_loop_init ( event_handler , NULL ) ) ;
+	esp_event_loop_init(event_handler, NULL);
+	wifi_init_config_t wifi_config = WIFI_INIT_CONFIG_DEFAULT ( ) ;
+	ESP_ERROR_CHECK ( esp_wifi_init ( & wifi_config ) ) ;
+	ESP_ERROR_CHECK ( esp_wifi_set_mode ( WIFI_MODE_STA ) ) ;
+	ESP_ERROR_CHECK ( esp_wifi_start ( ) ) ;
+	wifi_scan_config_t scan_config =  {
+		. ssid  =  0 ,
+		. bssid  =  0 ,
+		. channel  =  0 ,
+	        . show_hidden  =  true
+	} ;
+	ESP_ERROR_CHECK ( esp_wifi_scan_start ( & scan_config ,  true ) );
+	uint16_t ap_num = MAX_APs;
+	wifi_ap_record_t ap_records [ MAX_APs ];
+	ESP_ERROR_CHECK ( esp_wifi_scan_get_ap_records ( & ap_num , ap_records ) );
+	print_to_host("Found %d access points:\n", ap_num);
+		print_to_host("\n");
+		print_to_host("               SSID              | Channel | RSSI |   Auth Mode \n");
+		print_to_host("----------------------------------------------------------------\n");
+		for(int i = 0; i < ap_num; i++)
+			print_to_host("%32s | %7d | %4d | %12s\n", (char*)ap_records[i].ssid, ap_records[i].primary, ap_records[i].rssi, getAuthModeName(ap_records[i].authmode));
+		print_to_host("----------------------------------------------------------------\n");
+//	esp_apptrace_init();
     xTaskCreate(&blink_task, "blink_task", configMINIMAL_STACK_SIZE, NULL, 10, NULL);
 }
